e013b3a557e95fddbbf3f8ae395d2e16ba6cd3b2
Mclaren Ais C. Miranda <mobstaanimations@gmail.com>
2026-09-25 16:47:46 +0800

    commit

--- lib/routes/app_routes.dart
+++ lib/routes/app_routes.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import '../../screens/splash_screen.dart';
+import '../screens/splash_screen.dart';
 // import '../screens/login_screen.dart';
 // import '../screens/welcome_screen.dart';
 // import '../screens/select_room_screen.dart';
